8417f7d8bd4ec7eb903d438590341cce97697d0b
bjbomfim <abjbomfim@outlook.com>
2022-12-01 15:07:13 -0300

    Initial Commit

---
+++ RelaxJedi/RelaxJediApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelaxJediApp.swift
+//  RelaxJedi
+//
+//  Created by Alexandre Bomfim on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct RelaxJediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
